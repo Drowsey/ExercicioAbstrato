5c749fdae5985498bd3fd1b8d61563b282cff2c5
Drowsey <allenkevin.cardoso@gmail.com>
2021-07-31 20:11:32 -0300

    Adicionado as entidades, enum e parte do texto do console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using ExercicioAbstrato.Entities;
+using ExercicioAbstrato.Entities.Enums;
 
 namespace ExercicioAbstrato
 {
@@ -6,7 +9,35 @@ namespace ExercicioAbstrato
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Shape> shapeList = new List<Shape>();
+
+            Console.Write("Enter the number of shapes: ");
+            int numShape = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numShape; i++)
+            {
+                Console.Write($"Shape #{i} data:\nRectangle or circle? (r/c)");
+                string o = Console.ReadLine().ToLower();
+
+                if (o == "r" || o == "c")
+                {
+                    Console.Write("Color (Black/Blue/Red): ");
+                    Color c = Enum.Parse<Color>(Console.ReadLine());
+                    if (o == "r")
+                    {
+                        Console.Write("Width: ");
+                        double width = double.Parse(Console.ReadLine());
+                        Console.Write("Height: ");
+                        double height = double.Parse(Console.ReadLine());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Comando inválido! Digite 'r' ou 'c' ");
+                    i--;
+                    continue;
+                }
+            }
         }
     }
 }
